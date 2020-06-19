7194cbee849bb32def873e34da7f3c2267ec183c
stmum0107 <stmum0107@gmail.com>
2020-06-19 09:46:37 +0530

    added owners list and updaed the controller to populate the index page. closes #16

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,15 +1,26 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.sfgpetclinic.services.OwnerService;
 
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
+	
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
 
 	@RequestMapping({"","/", "/index", "/index.html"})
-	public String ListOwners() {
+	public String ListOwners(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
 		return "Owners/index";		
 	}
 }
