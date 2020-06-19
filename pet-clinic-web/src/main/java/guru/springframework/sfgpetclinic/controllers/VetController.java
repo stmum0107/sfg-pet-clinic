5d8fbab0da95191297c6e24e9629ad26b08b4783
stmum0107 <stmum0107@gmail.com>
2020-06-19 10:33:42 +0530

    Added the vets list to vets index page and updated the controller to populate the list. closes #15

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.sfgpetclinic.services.VetService;
 
 @Controller
 public class VetController {
 
+	private final VetService vetService;
+		
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
 	@RequestMapping({"/vets","/vets/index","/vets/index.html"})
-	public String ListVets() {
+	public String ListVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "Vets/index";
 	}
 }
