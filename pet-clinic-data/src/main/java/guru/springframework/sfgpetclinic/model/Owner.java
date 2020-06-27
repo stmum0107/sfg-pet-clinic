765dc185db91d84693dc4837401e8d2208a8cfad
stmum0107 <stmum0107@gmail.com>
2020-06-27 08:58:54 +0530

    Added visit and updated owner with pet relationship. closes #26

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,6 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
 
+	private Set<Pet> Pets;
+
+	public Set<Pet> getPets() {
+		return Pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		Pets = pets;
+	}
+	
+	
 	
 }
