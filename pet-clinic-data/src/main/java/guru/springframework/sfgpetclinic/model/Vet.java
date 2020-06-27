dcdf8cde02ba0a519a9da499f69c405c2b73860c
stmum0107 <stmum0107@gmail.com>
2020-06-27 09:03:36 +0530

    Added the Specialty object and added the relationship with Vet. closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+	
+	private Set<Specialty> specialities;
+
+	public Set<Specialty> getSpecialities() {
+		return specialities;
+	}
+
+	public void setSpecialities(Set<Specialty> specialities) {
+		this.specialities = specialities;
+	}
+	
+	
 
 }
